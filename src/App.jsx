83d6af4aa358030de587eb2935c1c6a2acee6f61
Pritam Barnwal <pritambarnwal629@gmail.com>
2024-09-21 03:06:42 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import HeroSection from "./components/Herosection/Hero";
 import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return(
     <>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
     </>
   );
 }
